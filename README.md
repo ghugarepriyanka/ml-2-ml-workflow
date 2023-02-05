e0a691af5259ec5f9408731cc9b9630026348585
ghugarepriyanka <ghugarepriyanka@gmail.com>
2023-02-05 20:19:09 +0800

    adding codes for ml worflow

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# ml-2-ml-workflow
+# Udacity-Developing-Your-First-ML-Workflow
+
+## Deploy and monitor a machine learning workflow for Image Classification
+
+### Dataset Prep 
+
+![S3_Train_Data.png](images/S3_Train_Data.png)
+
+![S3_Test_Data.png](images/S3_Test_Data.png)
+
+### Training Job 
+
+![Training_Job.png](images/Training_Job.png)
+
+### Endpoint Deployment
+
+![Img_Classification_Endpoint.png](images/Img_Classification_Endpoint.png)
+
+### 3 Lambdas for to take s3 image object key, serialise the s3 image object, get inference result and filter lower threshold results
+
+```Codes in lambda.py```
+
+1 - serializeImageData - Added permission for S3 Full Access to read S3 image objects
+2 - inferSerializedImageData - Added permission for Sagemaker Full Access to invoke sagermaker endpoint
+3 - filterResults - No extra permissions required
+
+### Step Function Graph
+
+![stepfunctions_graph.png](images/stepfunctions_graph.png)
+
+```State Machine config in MLWorkflowStateMachine.json```
+
+### State Machine Execution - Success
+
+![MLWorkflowStateMachine_Success.png](images/MLWorkflowStateMachine_Success.png)
+
+### State Machine Execution - Failure (Exception for lower threshold)
+
+![MLWorkflowStateMachine_Failure.png](images/MLWorkflowStateMachine_Failure.png)
+
+### Data Capture for Multiple Inferences
+
+![Captured_Data.png](images/Captured_Data.png)
+
+### Visualisation Plot
+
+![Visualization.png](images/Visualization.png)
+
+## License
+
+[License](LICENSE.txt)
---
+++ lambda.py
@@ -0,0 +1,118 @@
+"""
+serializeImageData: Pull an image from S3 and return serialized image data
+"""
+import json
+import boto3
+import base64
+
+#For error handling
+import botocore
+
+s3 = boto3.resource('s3')
+
+def lambda_handler(event, context):
+    """A function to serialize target data from S3"""
+    
+    # Get the s3 address from the Step Function event input
+    key = event['s3_key']
+    bucket = event['s3_bucket']
+    
+    # Download the data from s3 to /tmp/image.png
+    try:
+        s3.Bucket(bucket).download_file(key, '/tmp/image.png')
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            print("The object does not exist.")
+        else:
+            raise
+
+    # We read the data from a file
+    with open("/tmp/image.png", "rb") as f:
+        image_data = base64.b64encode(f.read())
+
+    # Pass the data back to the Step Function
+    print("Event:", event.keys())
+    return {
+        'statusCode': 200,
+        'body': {
+            "image_data": image_data,
+            "s3_bucket": bucket,
+            "s3_key": key,
+            "inferences": []
+        }
+    }
+
+
+
+"""
+inferSerializedImageData: Using image data, invoke img classification model endpoint and return inference
+"""
+import json
+import base64
+import boto3
+
+# Name of the deployed model
+ENDPOINT = "image-classification-2023-02-05-08-27-45-580"
+runtime= boto3.client('runtime.sagemaker')
+
+def lambda_handler(event, context):
+
+    # Decode the image data
+    image = base64.b64decode(event['image_data'])
+
+    # Instantiate a Predictor
+    response = runtime.invoke_endpoint(EndpointName=ENDPOINT, ContentType='application/x-image', Body=image)
+    inferences = response['Body'].read().decode('utf-8')
+    event["inferences"] = [float(x) for x in inferences[1:-1].split(',')]
+    
+    # We return the data back to the Step Function    
+    return {
+        'statusCode': 200,
+        'body': {
+            "image_data": event['image_data'],
+            "s3_bucket": event['s3_bucket'],
+            "s3_key": event['s3_key'],
+            "inferences": event['inferences'],
+        }
+    }
+
+
+"""
+filterResults: Filter out inference below threshold to return inferences when any inference is greater than THRESHOLD, throw exception when lower than THRESHOLD
+"""
+import json
+
+
+THRESHOLD = .93
+
+class MyException(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
+def lambda_handler(event, context):
+    
+    # Grab the inferences from the event
+    inferences = event['inferences']
+    
+    # Check if any values in our inferences are above THRESHOLD
+    meets_threshold = any (x >= THRESHOLD for x in inferences)
+    
+    # If our threshold is met, pass our data back out of the
+    # Step Function, else, end the Step Function with an error
+    if meets_threshold:
+        pass
+    else:
+        raise MyException("THRESHOLD_CONFIDENCE_NOT_MET")
+
+    return {
+        'statusCode': 200,
+        'body': {
+            "image_data": event['image_data'],
+            "s3_bucket": event['s3_bucket'],
+            "s3_key": event['s3_key'],
+            "inferences": event['inferences'],
+        }
+    }
